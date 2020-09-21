391e210be94ca414c29ed67b6622171d54188c61
Amy <amyngph@gmail.com>
2020-09-21 08:08:00 -0700

    Create proportional wall follower (no distance)

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import rospy
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import PointCloud2, LaserScan
 from laser_line_extraction.msg import LineSegmentList
 import numpy as np
+import math
 
 def lineseg_dists(p, a, b):
     """Cartesian distance from point to line segment
@@ -36,12 +38,21 @@ def lineseg_dists(p, a, b):
 
     return np.hypot(h, c)
 
+def computeAngle(vec1, vec2):
+    unit_vec1 = vec1 / np.linalg.norm(vec1)
+    unit_vec2 = vec2 / np.linalg.norm(vec2)
+    dot_product = np.dot(unit_vec1, unit_vec2)
+    angle = np.arccos(dot_product)
+    sign = np.sign(np.cross(unit_vec1, unit_vec2))
+    return sign * angle
+
 class WallFollower():
     def __init__(self):
         rospy.init_node("wall_follower")
         rate = rospy.get_param('~rate', 10)
         self.update_rate = rospy.Rate(rate)
 
+        self.forward_vel =  rospy.get_param('~forward_vel', 0.1)
         self.follow_dist =  rospy.get_param('~follow_dist', 0.5)
         self.visualize =  rospy.get_param('~visualize', True)
 
@@ -49,22 +60,61 @@ class WallFollower():
             self.wallDetectionCB, queue_size=1)
         self.wall_detection_msg = LineSegmentList()
 
+        self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
     def wallDetectionCB(self, msg):
         self.wall_detection_msg = msg
 
-    def parseLineSegmentList(lsl_msg):
-        for msg in lsl_msg:
-            print(msg)
+    def parseLineSegmentList(self, lsl_msg):
+        starts, ends = [], []
+
+        for msg in lsl_msg.line_segments:
+            # Filter nan line segments
+            if math.isnan(msg.radius):
+                continue
+            starts.append(msg.start)
+            ends.append(msg.end)
+
+        return np.array(starts), np.array(ends)
 
     def findNearestWall(self, wall_msg):
-        # wall_starts =
-        # lineseg_dists(p, a, b)
-        pass
+        starts, ends = self.parseLineSegmentList(wall_msg)
+
+        if len(starts) == 0 and len(ends) == 0:
+            return [], []
+
+        distances = lineseg_dists((0,0), starts, ends)
+
+        idx = np.argmin(distances)
+        print(type(starts[idx]))
+        return starts[idx], ends[idx]
+
+    def computeAngularCommand(self, wall_start, wall_end):
+        robot_vec = np.array((1,0))
+
+        heading_offset_1 = computeAngle(robot_vec, wall_start - wall_end)
+        heading_offset_2 = computeAngle(robot_vec, wall_end - wall_start)
+
+        heading_offset = min(abs(heading_offset_1), (heading_offset_2))
+
+        # TODO: Make PID
+        ang_cmd = heading_offset * 0.4
+        return ang_cmd
+
 
     def run(self):
         while not rospy.is_shutdown():
-            self.findNearestWall(self.wall_detection_msg)
+            wall_start, wall_end = self.findNearestWall(self.wall_detection_msg)
 
+            # Check for wall detections
+            if len(wall_start) > 0:
+                ang_cmd = self.computeAngularCommand(wall_start, wall_end)
+                vel_cmd = self.forward_vel # TODO: make this not constant
+                twist_msg = Twist()
+                twist_msg.linear.x = vel_cmd
+                twist_msg.angular.z = ang_cmd
+
+                self.twist_pub.publish(twist_msg)
             self.update_rate.sleep()
 
 if __name__ == "__main__":
